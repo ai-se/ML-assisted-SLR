809fec04651cbe3390f51f4d3adcc0374d1e6373
Zhe Yu <zyu9@ncsu.edu>
2016-09-28 09:08:33 -0400

    a

--- Learning_assisted_review/src/run_Hall.sh
+++ Learning_assisted_review/src/run_Hall.sh
@@ -1,6 +1,6 @@
 #! /bin/tcsh
 
 foreach VAR (1 5)
-  bsub -q standard -W 5000 -n 5 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 5 /share2/zyu9/miniconda/bin/python2.7 runner_hpc.py repeat_Hall $VAR
+  bsub -q long -W 5000 -n 5 -o ./out/$VAR.out.%J -e ./err/$VAR.err.%J mpiexec -n 5 /share2/zyu9/miniconda/bin/python2.7 runner_hpc.py repeat_Hall $VAR
 end
 
